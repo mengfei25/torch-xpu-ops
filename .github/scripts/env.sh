95943231d588c9d24f13ddb6773ca546cb2a67eb
mengfeil <mengfei.li@intel.com>
2024-10-16 16:02:01 +0800

    debug

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-source ${HOME}/intel/oneapi/setvars.sh
+export ONEAPI_ROOT="${HOME}/intel/oneapi"
+source ${ONEAPI_ROOT}/compiler/latest/env/vars.sh
+source ${ONEAPI_ROOT}/pti/latest/env/vars.sh
+source ${ONEAPI_ROOT}/mkl/latest/env/vars.sh
+source ${ONEAPI_ROOT}/umf/latest/env/vars.sh
