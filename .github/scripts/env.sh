7cfb43820518c607003e03026b49027626f8429a
mengfeil <mengfei.li@intel.com>
2024-12-20 09:56:41 +0800

    0.5.3

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [ "$1" != "nightly_wheel" ];then
-    source ${HOME}/intel/oneapi/compiler/latest/env/vars.sh
-    source ${HOME}/intel/oneapi/umf/latest/env/vars.sh
-    source ${HOME}/intel/oneapi/pti/latest/env/vars.sh
+    source /home/gta/intel/oneapi/setvars.sh
 else
     echo "Don't need to source DL-Essential for nightly wheel"
 fi
